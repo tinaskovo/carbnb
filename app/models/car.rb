62426c3283ece9262d236989a41a3c32569acdd7
Fred Fooks <fredjfooks@gmail.com>
2018-05-14 17:32:54 +0200

    models validated and tested

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,28 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
+
+  validates :year_made, presence: true, numericality: true
+  validates :price, presence: true, numericality: true
+
+  validates_associated :bookings
+
+  validates :users,  presence: true
+  validates :make, presence: true
+  validates :car_model,  presence: true
+
+
+
+  validates :engine_size, numericality: true
+  validates :mileage, numericality: { greater_than: 1, message: "must be greater that zero"}
+  validates :seats, numericality: true
+  validates :doors, numericality: true
+  validates :bhp, numericality: true
+
+  validates :fuel_type, :inclusion => { :in => ["petrol", "diesel", "electric"],
+    message: "%{value} is not a valid fuel type" }, allow_nil: true
+
+  validates :transmission, presence: true, :inclusion => { :in => ["automatic" "semi-auto", "manual"],
+    message: "%{value} is not a valid fuel type" }, allow_nil: true
 end
