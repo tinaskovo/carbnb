399b0f620ae691d06ed9f0da03c8d5800374e2e3
Fred Fooks <fredjfooks@gmail.com>
2018-05-17 12:04:42 +0200

    sorted out seed images and avatars

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,11 +18,11 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name)
-    end
+  def user_params
+    params.require(:user).permit(:name)
+  end
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
   has_many :cars, dependent: :destroy
+  mount_uploader :photo, PhotoUploader
+
 end
