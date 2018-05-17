c8649e9d98c0df9ab9ff58cee74001247c2a7ab8
Magnus Gram <magnusdk@live.com>
2018-05-17 19:19:46 +0200

    Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :cars
   resources :bookings
+  resources :profiles
   get 'design', to: 'pages#design'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
