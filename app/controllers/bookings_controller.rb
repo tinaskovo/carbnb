9150dad7b7c96e9e9bbc6ee85bbf6399aae8759b
Fred Fooks <fredjfooks@gmail.com>
2018-05-17 15:48:14 +0200

    did some stuff

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,49 +1,28 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings = Booking.all
-  end
-
-  def show
-    @car = Car.new
-  end
-
   def new
     @car = Car.find(params[:car_id])
     @booking = Booking.new
   end
 
-  def edit
-  end
-
   def create
-    @car = Car.find(params[:car_id])
-    @booking = Booking.new(booking_params)
-    if @booking.save
-      redirect_to booking_path(@booking)
+    car = Car.find(params[:car_id])
+    booking = Booking.new(booking_params)
+    booking.car = car
+    booking.user = current_user
+
+    if booking.save
+      redirect_to booking_path(booking)
     else
       render :new
     end
   end
 
-  def update
-    @booking.update(booking_params)
-    redirect_to booking_path(@booking)
-  end
-
   def destroy
-    @booking = Booking.find(params[:id])
-    @car = @booking.car
-    @booking.destroy
-    redirect_to bookings_path
   end
 
   private
 
-    def booking_params
-      params.require(:booking).permit(:name)
-    end
-
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out, :user)
+  end
 end
