fe3c95461ea2535b8f3acaefeee8a17f175ebefd
Magnus Gram <magnusdk@live.com>
2018-05-18 12:35:34 +0200

    Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :bookings
   resources :profiles
   get 'design', to: 'pages#design'
+  get "test", to: "profiles#test"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
